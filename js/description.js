58780cfc199a77ec2651fc2d52b7ab146cf3cc22
Maria <zhmasha2505@gmail.com>
2022-11-21 12:40:18 +0200

    first commit

---
+++ js/description.js
@@ -0,0 +1,113 @@
+// Getting data from JSON file
+const data = require('./response.json');
+console.log(data);
+
+// Getting an id by item in index.html
+var myId = localStorage.getItem("id");
+console.log(`YOU GOT A VAR/DATA: ${myId}, it is a ${typeof (myId)}`);
+
+
+// Main populate function
+async function populate() {
+    /*Getting an API
+    const requestURL = 'https://';
+    const request = new Request(requestURL);
+    const response = await fetch(request);
+    const jobList = await response.json();
+*/
+
+const jobList = data;
+    // Finding index of clicked element
+    const myIndex = jobList.findIndex(item => `${item.id}` == `${myId}`);
+    console.log(`Clicked index is: ${myIndex}`);
+
+    // Other Functions
+    populateMain(jobList[myIndex]);
+    populateAside(jobList[myIndex]);
+}
+
+function populateMain(obj) {
+    ////Header
+    const myH2 = document.querySelector('h2#title');
+    myH2.textContent = obj.title;
+    const salaryP = document.querySelector('p#salary');
+    salaryP.textContent = `\u{20AC} ${obj.salary}`;
+    const postedDate = document.querySelector("p#postedAt");
+    postedDate.textContent = `Posted: ${obj.createdAt.slice(0, 10)}`;
+    
+    ////Body
+    const descrP = document.querySelector("p#descr");
+    descrP.textContent = obj.description;
+
+    ////Footer
+    //Employment
+    const divEmployment = document.querySelector("div#employment-type");
+    for (let index in obj.employment_type) {
+        const employmentEl = document.createElement('p');
+        employmentEl.className = "employment-type-el";
+        employmentEl.textContent = obj.employment_type[index];
+        divEmployment.appendChild(employmentEl);
+    }
+    //Benefits
+    const divBenefits = document.querySelector("div#benefits");
+    for (let index in obj.benefits) {
+        const benefitsEl = document.createElement('p');
+        benefitsEl.className = "benefits-el";
+        benefitsEl.textContent = obj.benefits[index];
+        divBenefits.appendChild(benefitsEl);
+    }
+
+    const divImages = document.querySelector("div#images");
+    for (let index in obj.pictures) {
+        const imgEl = document.createElement('img');
+        imgEl.className = "img-el object-cover";
+        async function getImg() {
+            const requestURL = obj.pictures[0];
+            const request = new Request(requestURL);
+            const response = await fetch(request);
+            imgEl.setAttribute("src", `${response.url} `);
+        }
+        getImg();
+        divImages.appendChild(imgEl);
+    }
+}
+
+    function populateAside(obj){
+        //converting coordinates
+        let myLat = obj.location.lat;
+        let myLong = obj.location.long;
+        console.log(`Your coordinates are: Lat: ${myLat} ${myLong}`);
+    
+        //Aside Content
+        const depNameP = document.querySelector('p#dep-name');
+        depNameP.textContent = `Department name: ${obj.name}`;
+    
+        const addressP = document.querySelector('p#address');
+        addressP.textContent = obj.address;
+    
+        const phoneP = document.querySelector('p#phone');
+        phoneP.textContent = obj.phone;
+    
+        const emailP = document.querySelector('p#email');
+        emailP.textContent = obj.email;
+     
+    // Map
+        const divMap = document.querySelector('div#map');
+        const myMap = document.createElement('iframe');
+        myMap.className = "m-auto rounded-b-lg"
+        myMap.setAttribute("width", "100%"); //402(fixed)
+        myMap.setAttribute("height", "85%"); //250(fixed)
+        myMap.setAttribute("frameborder", 0);
+        myMap.setAttribute("marginheight", 0);
+        myMap.setAttribute("marginwidth", 0);
+        myMap.setAttribute("scrolling", "no");
+        myMap.setAttribute("src", `https://maps.google.com/maps?q=${myLat}+${myLong}&hl=en&z=14&output=embed`);
+        divMap.appendChild(myMap);
+    }
+
+
+    populate();
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,112 @@
+// Getting data from JSON file
+const data = require('./response.json');
+console.log(data);
+
+// Main  populate function
+async function populate() {
+    // Getting API converting to json
+    // const requestURL = 'https:';
+    // const requestURL = '../response.json';
+    // const request = new Request(requestURL);
+    // const response = await fetch(request);
+    // jobList = await response.json();
+    
+    const jobList = data;
+    //Calling other functions
+    populateArticles(jobList);
+}
+
+function populateArticles(obj) {
+    const section = document.querySelector('section');
+    section.className = "space-y-2";
+
+    for (let item in obj) {
+        const myArticle = document.createElement('article');
+        myArticle.className = "md:flex-row flex flex-col-reverse drop-shadow-md p-6 justify-between hover:bg-gray-50 hover:shadow-xl transition-all duration-300";
+
+        //divContent
+        const divContent = document.createElement('div');
+        divContent.className = "gap-x-6 flex";
+        //Image 
+        const myImage = document.createElement('img');
+        // Getting different image
+        async function getImg() {
+            const requestURL = obj[item].pictures[0];
+            const request = new Request(requestURL);
+            const response = await fetch(request);
+            myImage.setAttribute("src", `${response.url} `);
+            myImage.className = "shrink-0 object-cover"
+        }
+        getImg();
+
+        // divText
+        const divText = document.createElement('div');
+        divText.className = "space-y-2";
+
+
+        // Title link element
+        const myAnchor = document.createElement('a');
+        myAnchor.textContent = obj[item].title;
+        myAnchor.className = "lg:font-bold lg:text-xl font-normal text-lg leading-6";
+        myAnchor.setAttribute("id", obj[item].id);
+        myAnchor.setAttribute("target", "_blank");
+        myAnchor.setAttribute("href", "descriptionPage.html");
+        myAnchor.setAttribute("onClick", `choose("${obj[item].id}")`);
+
+        //Department Name
+        const myName = document.createElement('p')
+        myName.className = "nameEl";
+        myName.textContent = `Department name \u{2022} ${obj[item].name}`;
+
+        // Address paragraph
+        const myPara1 = document.createElement('p');
+        myPara1.className = "flex flex-row-reverse justify-end items-baseline gap-x-2";
+        const iconLoc = document.createElement('i');
+        myPara1.textContent = `  ${obj[item].address}`;
+        iconLoc.className = "fa-solid fa-location-dot";
+
+        // divAside
+        const divAside = document.createElement('div');
+        divAside.className = "flex flex-col justify-between items-end shrink-0";
+        const bookLink = document.createElement('button');
+        bookLink.className = "hidden md:inline";
+        bookLink.href = "#";
+        const iconBook = document.createElement('i');
+        iconBook.className = "fa-regular fa-bookmark";
+
+        const postedDate = document.createElement("p");
+        postedDate.textContent = `Posted: ${obj[item].createdAt.slice(0, 10)}`;
+        postedDate.className = "text-sm md:text-base"
+
+        // Appending all
+        myArticle.appendChild(divContent);
+        divContent.appendChild(myImage);
+        divContent.appendChild(divText);
+
+        divText.appendChild(myAnchor);
+        divText.appendChild(myName);
+        divText.appendChild(myPara1);
+        myPara1.appendChild(iconLoc);
+
+        myArticle.appendChild(divAside);
+        divAside.appendChild(bookLink);
+        bookLink.appendChild(iconBook);
+        divAside.appendChild(postedDate);
+
+        section.appendChild(myArticle);
+    }
+}
+populate();
+
+// Saving clicked item id to send it to description.js
+let final;
+function choose(choise) {
+    final = choise;
+    localStorage.setItem("id", `${final}`);
+    location.href = "../html/descriptionPage.html"
+}
+
+
+
+
+
